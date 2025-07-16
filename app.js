967cf298e9fbec99a139c47ceb5833ef7ba1329b
LUJAINA ALI <lujainaali10@gmail.com>
2025-07-16 15:15:14 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let interval;
+function update(){
+   seconds++;
+   if(seconds===60){
+      seconds=0;
+      minutes++;
+   }
+   if(minutes===60){
+      minutes=0;
+      hours++;
+   }
+   document.getElementById("stopwatch").innerText=
+   (hours<10?'0':'')+hours+":"+
+   (minutes<10?'0':'')+minutes+":"+
+   (seconds<10?'0':'')+seconds;
+}
+function start(){
+   interval = setInterval(update,1000)
+}
+function stop(){
+   clearInterval(interval);
+}
+function reset(){
+   clearInterval(interval)
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    document.getElementById('stopwatch').innerText='00:00:00';
+}
